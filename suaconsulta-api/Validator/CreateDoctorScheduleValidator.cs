b45cb4913d1988fa50cec7c916bb9c1e40e6e9c6
Caio Micael <contatocaiomicael@gmail.com>
2025-05-01 00:41:56 -0300

    feat - add new validation to doctorSchedule.

--- suaconsulta-api/Validator/CreateDoctorScheduleValidator.cs
+++ suaconsulta-api/Validator/CreateDoctorScheduleValidator.cs
@@ -1,12 +1,17 @@
 ﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using suaconsulta_api.Data;
 using suaconsulta_api.DTO;
 
 namespace suaconsulta_api.Validator
 {
     public class CreateDoctorScheduleValidator : AbstractValidator<CreateDoctorScheduleDto>
     {
-        public CreateDoctorScheduleValidator()
+        private readonly AppDbContext context;
+
+        public CreateDoctorScheduleValidator(AppDbContext _context)
         {
+            context = _context ?? throw new ArgumentNullException(nameof(_context));
             RuleFor(x => x.StartTime)
                 .NotEmpty()
                     .WithMessage("Data é obrigatória")
@@ -14,6 +19,24 @@ namespace suaconsulta_api.Validator
                     .WithMessage("Data informada é menor que a data atual!")
                 .LessThan(x => x.EndTime)
                     .WithMessage("Data de inicio deve ser menor que a data de fim!");
+            RuleFor(x => x.DoctorId)
+                .MustAsync((DoctorId, CancellationToken) => ExistsDoctorAsync(DoctorId, CancellationToken))
+                     .WithMessage("Médico não cadastrado!");
+        }
+
+        /// <summary>
+        /// Verifica se o médico existe no banco de dados.
+        /// </summary>
+        /// <param name="PatientId">Id do médico</param>
+        /// <param name="cancellationToken">cancellation Token</param>
+        /// <returns>bool</returns>
+        private async Task<bool> ExistsDoctorAsync(int DoctorId, CancellationToken cancellationToken)
+        {
+            if (await context.Doctor.FirstOrDefaultAsync(D => D.Id == DoctorId) == null)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
