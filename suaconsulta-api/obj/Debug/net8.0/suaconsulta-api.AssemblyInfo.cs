a77bb9406eede7112fd3ec24c74311f1e1550018
CaioMicael <contatocaiomicael@gmail.com>
2025-05-07 17:32:53 -0300

    AssemblyInfo

--- suaconsulta-api/obj/Debug/net8.0/suaconsulta-api.AssemblyInfo.cs
+++ suaconsulta-api/obj/Debug/net8.0/suaconsulta-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("suaconsulta-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a371bd2e03eb5188e9e1ca34c4f565c1c946f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad438b14c667e35ff61cf4d0d785e5f9075db65")]
 [assembly: System.Reflection.AssemblyProductAttribute("suaconsulta-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("suaconsulta-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
