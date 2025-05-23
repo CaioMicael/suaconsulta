da89cbdaa64f9d871aac43504160eb9a0ab0474c
CaioMicael <contatocaiomicael@gmail.com>
2025-05-23 17:34:02 -0300

    feat - add prop size to Input component

--- suaconsulta-app/src/components/Input.tsx
+++ suaconsulta-app/src/components/Input.tsx
@@ -10,13 +10,25 @@ interface InputProps {
     required?: boolean;
     onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
     className?: string;
+    size?: number;
 }
 
 
-const Input = ({ labelDescription, type, name, placeholder = "", value, disabled = false, onChange, required = false, className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" }: InputProps) => {
+const Input = ({ 
+                    labelDescription, 
+                    type, 
+                    name, 
+                    placeholder = "", 
+                    value, 
+                    disabled = false, 
+                    onChange, 
+                    required = false, 
+                    size = 69,
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                }: InputProps) => {
     return (
         <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor={name}>{labelDescription}</label>
+            <label className={`block mb-2 text-sm font-medium text-gray-900 dark:text-white w-${size}`} htmlFor={name}>{labelDescription}</label>
             <input 
                 type={type} 
                 id={name} 
