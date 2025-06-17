5420000c769eb440bcdfd66de9d05e17a9648196
CaioMicael <contatocaiomicael@gmail.com>
2025-06-17 19:57:05 -0300

    docs - SucessAlert documentation.

--- suaconsulta-app/src/components/alerts/SucessAlert.tsx
+++ suaconsulta-app/src/components/alerts/SucessAlert.tsx
@@ -3,6 +3,12 @@ interface SucessAlertProps {
     onClose?: () => void;
 }
 
+/**
+ * Mensagem de sucesso. Deve ser usado para indicar que uma ação foi concluída com sucesso.
+ * @param message Mensagem que será exibida no alerta
+ * @param onClose Função que será chamada ao fechar o alerta
+ * @returns JSX.Element
+ */
 const SucessAlert = ({ message ="Inserido com sucesso!", onClose }: SucessAlertProps) => {
     return (
         <div className="fixed bottom-0 w-full p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
