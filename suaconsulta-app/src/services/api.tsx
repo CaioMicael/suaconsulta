2f94f7ba9b791f418774575bc84c5cd3bec84bf2
Caio Micael <contatocaiomicael@gmail.com>
2025-06-17 20:59:42 -0300

    feat - add authorization in api

--- suaconsulta-app/src/services/api.tsx
+++ suaconsulta-app/src/services/api.tsx
@@ -1,9 +1,27 @@
 import axios from "axios";
 
+/**
+ * função base para criar uma instância do axios com a URL base da API.
+ * Já coloca automaticamente o token de autenticação no header Authorization.
+ * @returns {AxiosInstance} - Instância do axios configurada com a URL base.
+ */
 const api = axios.create(
     {
         baseURL: "https://localhost:7156/api/"
     }
-)
+);
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token'); // ou sessionStorage
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api
