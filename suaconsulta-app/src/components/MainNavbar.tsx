d2bc3af476e5e2cb8360e086ba2500677fe94bfc
Caio Micael <contatocaiomicael@gmail.com>
2025-06-27 10:56:06 -0300

    feat - style for MainNavbar

--- suaconsulta-app/src/components/MainNavbar.tsx
+++ suaconsulta-app/src/components/MainNavbar.tsx
@@ -8,12 +8,12 @@ const MainNavbar = () => {
     const role = localStorage.getItem('role');
 
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
-            <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Sua Consulta</span>
-            </a>
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <nav className="border-1 bg-white border-gray-200 dark:bg-gray-900">
+            <div className="w-full relative flex h-16 items-center justify-between">
+              <a href="https://flowbite.com/" className="flex items-center space-x-2 rtl:space-x-reverse">
+                  <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                  <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Sua Consulta</span>
+              </a>
               <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                   {role === '2' && (
